a5ec776efc569dfa0f9bd0c4708210de39259559
kiritAyya <kirit.ayya@gmail.com>
2017-04-03 00:58:20 +0530

    Variable Bindings

--- functions.erl
+++ functions.erl
@@ -3,3 +3,14 @@
 
 head([H|_]) -> H.
 second([_,X|_]) -> X.
+
+same(X,X) ->
+	true;
+same(_,_) ->
+	false.
+
+valid_time({Date = {Y,M,D}, Time = {H,Min,S}}) ->
+	io:format("The Date tuple (~p) says today is: ~p/~p/~p,~n",[Date,Y,M,D]),
+	io:format("The Time tuple (~p) says the time is: ~p:~p:~p,~n",[Time,H,Min,S]);
+valid_time(_) ->
+ 	io:format("Provided data is wrong!~n").
