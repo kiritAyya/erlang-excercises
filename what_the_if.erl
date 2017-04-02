2d9f66ecfd815b5a529a9a2bdf1e6b029a3627ab
kiritAyya <kirit.ayya@gmail.com>
2017-04-03 01:16:35 +0530

    If/else expression

--- what_the_if.erl
+++ what_the_if.erl
@@ -1,5 +1,5 @@
 -module(what_the_if).
--export([heh_fine/0, oh_god/1]).
+-export([heh_fine/0, oh_god/1, help_me/1]).
 
 heh_fine() ->
 	if 1 =:= 1 ->
@@ -16,3 +16,11 @@ oh_god(N) ->
 	if N =:= 2 -> might_succeed;
 	true -> always_does
 end.
+
+help_me(Animal) ->
+	Talk = if Animal == cat -> "meow";
+			  Animal == cow -> "moo" ;
+			  Animal == dog -> "woof";
+			  true -> "jkasndjkasdn"
+			end,
+	{ Animal, "says " ++ Talk ++ " !"}.
